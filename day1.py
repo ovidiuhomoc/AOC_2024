8f9696353657393e8277f10b3c76d44b23e3f62c
ovidiu.homoc <ovidiu.homoc@tapeark.com>
2024-12-01 15:56:41 +1100

    day 1 prep

--- day1.py
+++ day1.py
@@ -1,5 +1,23 @@
+from typing import Any
+
 import day1
-import utils
 from utils.AOCParser import AOCParser
 
-aocp = AOCParser(day1.demoinput1, day1.extract_number)
+
+def header_parser(header: str) -> Any:
+    pass
+
+
+def row_parser(row: str) -> Any:
+    pass
+
+
+def main(aocp: AOCParser):
+    pass
+
+
+if __name__ == '__main__':
+    aocp = AOCParser(input_str=day1.demoinput,
+                     row_parser=row_parser,
+                     has_header=False,
+                     header_parser=None)
