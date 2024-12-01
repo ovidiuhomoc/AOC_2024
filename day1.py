499a3516e3f43445dd6f2b08a3c7310fc31e9f66
ovidiu.homoc <ovidiu.homoc@tapeark.com>
2024-12-01 16:21:36 +1100

    part 1 and part 2 finished

--- day1.py
+++ day1.py
@@ -1,3 +1,4 @@
+from collections import Counter
 from typing import Any
 
 import day1
@@ -8,18 +9,56 @@ def header_parser(header: str) -> Any:
     pass
 
 
-def row_parser(row: str) -> Any:
-    pass
+def row_parser(row: str) -> tuple[int, int]:
+    parsed_row = tuple(map(int, str(row).strip().split()))
+    return parsed_row[0], parsed_row[1]
 
 
 def part1(aocp: AOCParser):
-    pass
+    aocp.parse()
+
+    list_1: list = [row[0] for row in aocp.rows]
+    list_2: list = [row[1] for row in aocp.rows]
+
+    list_1.sort(reverse=False)
+    list_2.sort(reverse=False)
+
+    total_dif = 0
+
+    for l1_el, l2_el in zip(list_1, list_2):
+        diff = abs(l1_el - l2_el)
+        total_dif += diff
+
+    return total_dif
+
+
+def part2(aocp: AOCParser):
+    aocp.parse()
+
+    list_1: list = [row[0] for row in aocp.rows]
+    list_2: list = [row[1] for row in aocp.rows]
+
+    c = Counter(list_2)
+
+    total_sum = 0
+
+    for l1_el in list_1:
+        multip = c[l1_el] if c and l1_el in c else 0
+        total_sum += l1_el * multip
+
+    return total_sum
 
 
 if __name__ == '__main__':
-    aocp = AOCParser(input_str=day1.demoinput,
+    # aocp = AOCParser(input_str=day1.demoinput,
+    #                  row_parser=row_parser,
+    #                  has_header=False,
+    #                  header_parser=None)
+
+    aocp = AOCParser(input_str=day1.input1,
                      row_parser=row_parser,
                      has_header=False,
                      header_parser=None)
 
-    print(f"{part1(aocp) = }")
+    # print(f"{part1(aocp) = }")
+    print(f"{part2(aocp) = }")
